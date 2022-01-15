fdad48e2d4a4c2d3c8294e0a683e1390b1502e81
runru1030 <runru1030@gmail.com>
2022-01-15 22:58:10 +0900

    [push]

--- src/Router.js
+++ src/Router.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import Letter from "./Letter";
 import Main from "./Main";
 import Post from "./Post";
 
 const AppRouter = () => {
   return (
-    <BrowserRouter >
+    <HashRouter >
       <Routes>
         <Route path="/DontGoC1" element={<Main />} />
         <Route path="/DontGoC1/post" element={<Post />} />
         <Route path="/DontGoC1/letter" element={<Letter />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 export default AppRouter;
